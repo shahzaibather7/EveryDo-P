ccf73d1de4a109517bfcca1c70d29094b30f1b56
Shahzaib Ather <shahzaibather7@github.ca>
2017-08-08 11:13:55 -0700

    Initial Commit

---
+++ everydo p*/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  everydo p*
+//
+//  Created by Mohammad Shahzaib Ather on 2017-08-08.
+//  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ everydo p*/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  everydo p*
+//
+//  Created by Mohammad Shahzaib Ather on 2017-08-08.
+//  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
